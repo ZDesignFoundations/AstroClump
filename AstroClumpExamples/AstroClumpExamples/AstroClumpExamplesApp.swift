d5cfecc3bfcea253d870a8cfede53faba22b527c
Alex Hertzog <alexhe@zillowgruop.com>
2025-06-30 10:42:39 -0500

    Added new package structure and Examples xcode project.

---
+++ AstroClumpExamples/AstroClumpExamples/AstroClumpExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstroClumpExamplesApp.swift
+//  AstroClumpExamples
+//
+//  Created by Alex Hertzog on 6/30/25.
+//
+
+import SwiftUI
+
+@main
+struct AstroClumpExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
