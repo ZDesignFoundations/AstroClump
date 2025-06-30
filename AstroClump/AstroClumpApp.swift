6854e2b8eaa65eea4948e3fc412b99c8487719db
Alex Hertzog <alexhe@zillowgruop.com>
2025-06-30 08:11:49 -0500

    Initial Commit

---
+++ AstroClump/AstroClumpApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstroClumpApp.swift
+//  AstroClump
+//
+//  Created by Alex Hertzog on 6/30/25.
+//
+
+import SwiftUI
+
+@main
+struct AstroClumpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AstroClumpTests/AstroClumpTests.swift
@@ -0,0 +1,17 @@
+//
+//  AstroClumpTests.swift
+//  AstroClumpTests
+//
+//  Created by Alex Hertzog on 6/30/25.
+//
+
+import Testing
+@testable import AstroClump
+
+struct AstroClumpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
